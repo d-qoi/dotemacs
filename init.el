37550b56aa4f60c537748f00404a81870887f821
Alex Hirschfeld <Alex@d-qoi.com>
2023-04-17 14:36:48 -0500

    Removing old files, updating new files, branched exwm to exwm

--- init.el
+++ init.el
@@ -1,48 +1,22 @@
-(setq debug-on-error t)
+;;; init.el --- -*- lexical-binding: t -*-
 
 (setq custom-file "~/.emacs.d/emacs-custom.el")
-
-;; package setup
-(require 'package)
-(add-to-list 'package-archives
-         '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives
-             '("org" . "http://orgmode.org/elpa/") t)
-(package-initialize)
-(when (not package-archive-contents)
-    (package-refresh-contents))
-
-;; use-package setup
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-(require 'use-package)
-(use-package diminish
-  :ensure t)
-
 (add-to-list 'load-path "~/.emacs.d/elisp")
 
+;; package setup
+
+;; use-package setup
+
+
+
 ;; Emacs customizations that do not require installations
-(require 'init-defaults)
-(require 'init-default-packages)
-(require 'custom-functions)
+
 
 ;; submodules that may or may not be loaded
-(require 'init-submodules)
-(message "init-submodules")
-(require 'init-modes)
-(message "init-modes")
-(require 'init-helm)
-(message "init-helm")
-(require 'init-company)
-(message "init-company")
-(require 'init-magit)
-(message "init-magit")
-
-(require 'init-exwm) ;; the special cases
-(message "init-exwm")
 
 ;; One of the last things to do.
-(load custom-file)
+(if (file-exists-p custom-file)
+    (load custom-file))
 
 ;; and a prompt to save all unsaved customizations
 (add-hook 'kill-emacs-query-functions
